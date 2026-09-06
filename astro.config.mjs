208b31f6b1ee72025645f9b60430ebb5ca7ba51d
allthingsbend <hello@allthingsbend.org>
2026-09-06 11:44:50 -0700

    Move site to .com and improve SEO architecture

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://everyonesanathlete.app',
+  site: 'https://everyonesanathlete.com',
   integrations: [sitemap()],
 });
